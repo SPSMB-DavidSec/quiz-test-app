25a16cb9aad007cddd0ee9d4a1ecf0e4f016faba
David Šec <144169715+SPSMB-DavidSec@users.noreply.github.com>
2026-03-27 12:39:27 +0000

    init

---
+++ questions.js
@@ -0,0 +1,52 @@
+window.questionsData = [
+  {
+  "type": "multi",
+  "q": "3. Which of the following animals are classified as mammals? (Choose two.)",
+  "options": [
+    "Dolphin",
+    "Eagle",
+    "Elephant",
+    "Crocodile",
+    "Frog"
+  ],
+  "answer": [
+    0,
+    2
+  ]
+},
+{
+  "type": "single",
+  "q": "2. Which planet in our solar system is known for its prominent ring system?",
+  "options": [
+    "Mars",
+    "Saturn",
+    "Venus",
+    "Jupiter"
+  ],
+  "answer": [
+    1
+  ]
+},
+{
+  "type": "match",
+  "q": "1. Match each famous novel with its correct author.",
+  "left": [
+    "1984",
+    "Pride and Prejudice",
+    "The Hobbit",
+    "The Great Gatsby"
+  ],
+  "right": [
+    "George Orwell",
+    "Jane Austen",
+    "J.R.R. Tolkien",
+    "F. Scott Fitzgerald"
+  ],
+  "answer": {
+    "1984": "George Orwell",
+    "Pride and Prejudice": "Jane Austen",
+    "The Hobbit": "J.R.R. Tolkien",
+    "The Great Gatsby": "F. Scott Fitzgerald"
+  }
+}
+];
